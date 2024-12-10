0b4e1f3b42ba6199b6ffd81b05a12e6c488d204c
Mohamed Hashem <mohamed.hashem@halan.com>
2024-12-10 13:56:13 +0200

    maybe?

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"flag"
 	"fmt"
 	"net"
-	"net/http"
+	"os"
 	"os/exec"
 	"os/signal"
 	"runtime"
@@ -13,47 +15,53 @@ import (
 	"time"
 )
 
-func connect(path string) *http.Client {
-	client := http.Client{
-		Transport: &http.Transport{
-			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-				conn, err := net.Dial("unix", path)
-				if err != nil {
-					panic(err)
-				}
-				return conn, nil
-			},
-		},
-	}
-	return &client
-}
-
 func main() {
 	c, cancel := signal.NotifyContext(context.TODO(), syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
-	// filePath := flag.String("file", "", "file path to play")
+	filePath := flag.String("file", "", "file path to play")
+	cooldown := flag.Int("cooldown", 5, "cooldown for mpv to open")
 	// ips := flag.String("ips", "", "comma seprated list of IPs to connect to")
-	mpvSocket := "/tmp/mpvsocket.sock"
+	flag.Parse()
+	mpvSocket := "/tmp/mpvsocket"
 	if strings.Contains(runtime.GOOS, "windows") {
 		mpvSocket = "\\\\.\\pipe\\mpvsocket"
 	}
 
-	cmd := exec.CommandContext(c, "mpv", "--input-ipc-server="+mpvSocket, "/mnt/hdd/Movies/meat.mp4")
+	cmd := exec.CommandContext(c, "mpv", "--input-ipc-server="+mpvSocket, *filePath)
 	defer cmd.Cancel()
+	// cmd.Stdin = os.Stdin
 	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
-	// output, _ := cmd.StdoutPipe()
-	req := `{ "command": ["get_property", "playback-time"] }`
-	go func() {
-		time.Sleep(10 * time.Second)
-		client := connect(mpvSocket)
-		client.Do(&http.Request{})
-
-	}()
-
-	err := cmd.Run()
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
 	if err != nil {
 		panic(err)
 	}
-	time.Sleep(time.Second)
+
+	time.Sleep(time.Duration(*cooldown) * time.Second)
+
+	_, err = os.Stat(mpvSocket)
+	if os.IsNotExist(err) {
+		panic(err)
+	}
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(c, "unix", mpvSocket)
+	if err != nil {
+		panic(err)
+	}
+	responseScanner := bufio.NewScanner(conn)
+
+	// { "command": ["set_property", "pause", true] }
+	// req := `{ "command": ["observe_property_string", 1, "pause"] }` + "\n"
+	// req := `{ "command": ["observe_property_string", 1, "playback-time"] }` + "\n"
+	req := `{ "command": ["get_property", "filename"] }` + "\n"
+	_, err = conn.Write([]byte(req))
+
+	go func() {
+		for responseScanner.Scan() {
+			fmt.Println(responseScanner.Text())
+		}
+	}()
+
+	err = cmd.Wait()
+	fmt.Println(err)
 }
